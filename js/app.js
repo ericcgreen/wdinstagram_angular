3341a02406f01731bfdf43928eaf714b7b77f26c
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-08 14:30:51 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+let data = [
+  {photo_url: "https://pbs.twimg.com/profile_images/786681705981673472/T5OKNZ1-.jpg", author: "Eric", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "},
+  {photo_url: "https://pbs.twimg.com/profile_images/786681705981673472/T5OKNZ1-.jpg", author: "Eva", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "},
+  {photo_url: "https://pbs.twimg.com/profile_images/786681705981673472/T5OKNZ1-.jpg", author: "Andy", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "}
+]
+
+
+angular
+  .module("wdinstagram", [])
+  .controller("wdiCtrl", [wdinstagramController])
+
+function wdinstagramController(){
+  this.instas = data
+}
